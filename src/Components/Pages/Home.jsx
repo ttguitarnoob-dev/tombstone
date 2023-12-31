c047a3e4598fb8a2136103b1c65ee6c5897a4027
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-12-31 11:35:53 -0600

    added title section and changed favicon

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -2,12 +2,19 @@ import BgImage from "../UI Components/BgImage";
 import ContactForm from "../UI Components/ContactForm";
 import Hero from "../../Images/Hero.webp"
 import InfoSection from "../UI Components/InfoSection";
+import Relettering from "../../Images/Relettering.webp"
 
 export default function Home() {
 
     return(
         <section>
             <BgImage imgPath={Hero} />
+            <InfoSection
+            title="Headstone Cleaning Services In Bastrop County, TX"
+            text="Welcome to Majestic Monuments! If you have a memorial that needs attention in Bastrop County, TX, this is your best bet! Feel free to browse our services and get in touch if you have any questions. "
+            imgPath={Relettering}
+            bgColor="linear-gradient(90deg, rgba(89,58,96,1) 0%, rgba(105,86,107,1) 68%, rgba(250,217,247,1) 100%)"
+            />
             <ContactForm />
         </section>
     )
