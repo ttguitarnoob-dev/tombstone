a1cdd03750cb7d9b8d36aac0bdf4099a58c708b1
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-12-31 13:52:52 -0600

    updated contact wording

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -9,7 +9,7 @@ export default function Contact() {
             
             
             <InfoSection
-                text="Looking to give your loved one's memorial some extra care? Majestic Monuments is here for you! Please reach out via the form below to let me know how I can provide my expert headstone cleaning that restores their memorial with a personal touch. "
+                text="Looking to give your loved one's memorial some extra care? Majestic Monuments is here for you! Please reach out via the form below. "
                 title="Contact"
                 bgColor="linear-gradient(90deg, rgba(89,58,96,1) 0%, rgba(105,86,107,1) 68%, rgba(250,217,247,1) 100%)"
                 imgPath={Filler}
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -2,7 +2,7 @@ import BgImage from "../UI Components/BgImage";
 import ContactForm from "../UI Components/ContactForm";
 import Hero from "../../Images/Hero.webp"
 import InfoSection from "../UI Components/InfoSection";
-import Relettering from "../../Images/Relettering.webp"
+import Logo from "../../Images/Logo.webp"
 
 export default function Home() {
 
@@ -12,7 +12,7 @@ export default function Home() {
             <InfoSection
             title="Headstone Cleaning Services In Bastrop County, TX"
             text="Welcome to Majestic Monuments! If you have a memorial that needs attention in Bastrop County, TX, this is your best bet! Feel free to browse our services and get in touch if you have any questions. "
-            imgPath={Relettering}
+            imgPath={Logo}
             bgColor="linear-gradient(90deg, rgba(89,58,96,1) 0%, rgba(105,86,107,1) 68%, rgba(250,217,247,1) 100%)"
             />
             <ContactForm />
