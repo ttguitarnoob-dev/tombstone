d86c9bcadc883886153d3e7e10e1c8695a86873e
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2023-12-31 11:05:24 -0600

    contact form looks good on mobile now

--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import BgImage from "../UI Components/BgImage";
 import ContactForm from "../UI Components/ContactForm";
 import Hero from "../../Images/Hero.webp"
+import InfoSection from "../UI Components/InfoSection";
 
 export default function Home() {
 
--- src/Components/UI Components/ContactForm.jsx
+++ src/Components/UI Components/ContactForm.jsx
@@ -1,23 +1,33 @@
-import { Input, Textarea } from "@nextui-org/react"
+import { Button, Input, Textarea } from "@nextui-org/react"
 
 export default function ContactForm() {
 
     return (
-        <section className="section-bg p-20 w-1/2 flex flex-col gap-4 justify-center">
+        <section className="section-bg p-8 w-1/2 flex flex-col gap-4 justify-center">
             <h1>Contact Us</h1>
             <p>Hello! We'd love to hear from you. Just fill out the form below, and we'll get back to you in a snap! </p>
-            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-                <Input type="text" label="Your Name" />
-                <Input type="email" label="Email" />
-            </div>
-            <div className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-                <Input type="phone" label="Phone Number" />
-                <Input type="text" label="Graveyard Location" />
-            </div>
-            <Textarea
-                label="How Can We Help?"
-                className="max-w-xs"
-            />
+            <form>
+                <div className="form-part flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                    <Input type="text" label="Your Name" />
+                    <Input type="email" label="Email" />
+                </div>
+                <div className="form-part flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
+                    <Input type="phone" label="Phone Number" />
+                    <Input type="text" label="Graveyard Location" />
+                </div>
+                <div className="form-part">
+                    <Textarea
+                        label="How Can We Help?"
+                        className="max-w-xs"
+                    />
+                </div>
+                <div className="form-part">
+                    <Button>
+                        Submit
+                    </Button>
+                </div>
+            </form>
+            
         </section>
     )
 }
