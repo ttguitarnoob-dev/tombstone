65831f4614ae5bbc3837d5cc77e092e091f11f69
ttguitarnoob-dev <ttguitarnoob@gmail.com>
2024-03-09 12:24:24 -0600

    added phone

--- src/Components/Pages/Contact.jsx
+++ src/Components/Pages/Contact.jsx
@@ -6,23 +6,24 @@ export default function Contact() {
     return (
         <section>
             <section className="home-bio">
-            <div className="text-paragraph">
-                <h1 className="page-title">Contact</h1>
-                <p>Looking to give your loved one's memorial some extra care? Majestic Monuments is here for you!</p>
-            </div>
-            <div>
-                <Image
-                src="https://www.kindpng.com/picc/m/156-1568226_transparent-contact-icon-png-contact-us-icon-png.png"
-                alt="contact"
-                width="200"
-                loading="lazy"
-                />
-            </div>
+                <div className="text-paragraph">
+                    <h1 className="page-title">Contact</h1>
+                    <p>Looking to give your loved one's memorial some extra care? Majestic Monuments is here for you!</p>
+                </div>
+                <div>
+                    <Image
+                        src="https://www.kindpng.com/picc/m/156-1568226_transparent-contact-icon-png-contact-us-icon-png.png"
+                        alt="contact"
+                        width="200"
+                        loading="lazy"
+                    />
+                </div>
             </section>
-            
-            
-            
-            
+                <div><p>Contact: phone</p></div>
+
+
+
+
 
             <ContactForm />
         </section>
